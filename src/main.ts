a839b8b927be9ca39e0786d953969b968579e407
20250508 정인식 <insik1109@gmail.com>
2025-07-04 22:56:17 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
+  // 정확히 이대로 설정 (정적파일 제공)
   app.useStaticAssets(join(process.cwd(), 'client/build'), {
     index: 'index.html',
     prefix: '/',
