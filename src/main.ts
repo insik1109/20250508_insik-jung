0230e228fdf9ace14945f4e9dd465ea342338b9f
20250508 정인식 <insik1109@gmail.com>
2025-07-04 23:16:47 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,10 @@
+// main.ts (정적파일 코드 제거한 버전)
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
-
-  // 정확히 이대로 설정 (정적파일 제공)
-  app.useStaticAssets(join(process.cwd(), 'client/build'), {
-    index: 'index.html',
-    prefix: '/',
-  });
-
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
